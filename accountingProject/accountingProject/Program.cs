4bb6ceacad3db75e8e89d618f6511858c6fec309
Brandon Roman <116917326+flipsun@users.noreply.github.com>
2023-02-01 11:36:09 -0800

    Added accountingProject assignment

---
+++ accountingProject/accountingProject/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace accountingProject
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting systems " +
+                "\n remember, we're\"accounting \" on you!");
+            Console.ReadLine();
+        }
+    }
+}
