a2d8b4c18533af94414ba836a69ac0cf9230f52c
Brandon Roman <116917326+flipsun@users.noreply.github.com>
2023-02-03 13:52:17 -0800

    Updated Branching Submission Assignment

--- BranchingSubmissionAssignment/BranchingSubmissionAssignment/Program.cs
+++ BranchingSubmissionAssignment/BranchingSubmissionAssignment/Program.cs
@@ -23,39 +23,40 @@ namespace BranchingSubmissionAssignment
             {
                 Console.WriteLine("Package too heavy to be shipped via Package Express. Have a good day.");
             }
-
-            // Prints message, then converts response to int "pckgWidth"
-            Console.WriteLine("Please enter the width of the package.");
-            int pckgWidth = Convert.ToInt32(Console.ReadLine());
-
-            // Prints message, then converts response to int "pckgHeight"
-            Console.WriteLine("Please enter the height of the package.");
-            int pckgHeight = Convert.ToInt32(Console.ReadLine());
-
-            // Prints message, then converts response to int "pckgLength"
-            Console.WriteLine("Please enter the length of the package.");
-            int pckgLength = Convert.ToInt32(Console.ReadLine());
-
-            // If total of pckgWidth, pckgHeight and pckgLength is greater than 50, prints message in braces 
-            if (pckgWidth + pckgHeight + pckgLength > 50)
-            {
-                Console.WriteLine("Package too big to be shipped via Package Express");
-            }
             else
             {
-                // dimensions = width multiplied by height multiplied  by length
-                int dimensions = pckgWidth * pckgHeight * pckgLength;
-                // dimWeight = dimensions multiplied by weight
-                int dimWeight = dimensions * pckgWeight;
-                // result = dimWeight divided by 100
-                int result = dimWeight / 100;
-                // Prints message, followed by the result int
-                Console.WriteLine("Your estimated total for shipping this package is: $" + result);
-                Console.WriteLine("Thank you!");
+                // Prints message, then converts response to int "pckgWidth"
+                Console.WriteLine("Please enter the width of the package.");
+                int pckgWidth = Convert.ToInt32(Console.ReadLine());
+
+                // Prints message, then converts response to int "pckgHeight"
+                Console.WriteLine("Please enter the height of the package.");
+                int pckgHeight = Convert.ToInt32(Console.ReadLine());
+
+                // Prints message, then converts response to int "pckgLength"
+                Console.WriteLine("Please enter the length of the package.");
+                int pckgLength = Convert.ToInt32(Console.ReadLine());
+
+                // If total of pckgWidth, pckgHeight and pckgLength is greater than 50, prints message in braces 
+                if (pckgWidth + pckgHeight + pckgLength > 50)
+                {
+                    Console.WriteLine("Package too big to be shipped via Package Express");
+                }
+
+                else
+                {
+                    // dimensions = width multiplied by height multiplied  by length
+                    int dimensions = pckgWidth * pckgHeight * pckgLength;
+                    // dimWeight = dimensions multiplied by weight
+                    int dimWeight = dimensions * pckgWeight;
+                    // result = dimWeight divided by 100
+                    int result = dimWeight / 100;
+                    // Prints message, followed by the result int
+                    Console.WriteLine("Your estimated total for shipping this package is: $" + result);
+                    Console.WriteLine("Thank you!");
+                }
             }
             Console.ReadLine();
-
-
 
         }
     }
