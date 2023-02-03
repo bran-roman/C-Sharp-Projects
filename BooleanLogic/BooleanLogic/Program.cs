f1a3182d0bd86954a884c5f78301add57b676e61
Brandon Roman <116917326+flipsun@users.noreply.github.com>
2023-02-02 16:59:22 -0800

    Added Boolean Logic Assignment

---
+++ BooleanLogic/BooleanLogic/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace BooleanLogic
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your age?");
+            string Age = Console.ReadLine();
+            int userAge = Convert.ToInt32(Age);
+            bool oldEnough = (userAge > 15);
+
+            Console.WriteLine("Have you ever had a DUI?");
+            string response = Console.ReadLine();
+            bool Dui = Convert.ToBoolean(response);
+            bool noDui = (true && (true || false));
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            string speeding = Console.ReadLine();
+            int tickets = Convert.ToInt32(speeding);
+
+            Console.WriteLine("Qualified?");
+            bool isQualified = (oldEnough && noDui && tickets < 4);
+            Console.WriteLine(isQualified);
+            Console.ReadLine();
+           
+        }
+    }
+}
