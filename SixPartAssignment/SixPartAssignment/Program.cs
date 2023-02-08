71d7d239b0ba81f099a4d38aa3e4f926761c9c26
Brandon Roman <116917326+flipsun@users.noreply.github.com>
2023-02-08 12:25:13 -0800

    Adding Console App Six Part Assignment

--- SixPartAssignment/SixPartAssignment/Program.cs
+++ SixPartAssignment/SixPartAssignment/Program.cs
@@ -23,161 +23,159 @@ namespace SixPartAssignment
             for (int m = 0; m < messages.Length; m++)
             {
                 messages.Append(userInput);
-                Console.WriteLine("Thank you. We've added your input to our string.");
             }
-            Console.ReadLine();
 
             for (int m = 0; m < messages.Length; m++)
             {
-                
+
                 Console.WriteLine(messages[m] + userInput);
                 Console.ReadLine();
-    
             }
-            
 
-            //if (messages[m] == "Hey there, ")
-            //{
-            //    Console.WriteLine(messages[m] + userInput);
+            // Part Two Assignment
 
+            // 1. / 2. Add infinite loop + Save code
+
+            // Asks user to enter integer
+            Console.WriteLine("Enter an integer: ");
+            // Converts user response to Int
+            int intNum = Convert.ToInt32(Console.ReadLine());
+            // Converts int to Boolean
+            bool intDoubleNum = Convert.ToBoolean(intNum);
+
+
+            do
+            {
+                Console.WriteLine("The double of " + intNum + " is " + intNum * 2);
+
+            }
+
+            // 3. Fix infinite loop so that it will execute properly
+
+
+            // When true, print this message (infinite loop)
+            // Adding ! to "intDoubleNum" to execute while statement, followed by "Console.ReadLine()" to pause console
+            while (!intDoubleNum);
+            {
+                Console.WriteLine("Great, let's move on.");
+            }
+            Console.ReadLine();
+
+            // Part Three Assignment
+            // 1. Add loop where "<" operator is the comparison used to determine whether to continue iterating loop
+            List<int> finalScores = new List<int>();
+            finalScores.Add(97);
+            finalScores.Add(86);
+            finalScores.Add(30);
+            finalScores.Add(25);
+            finalScores.Add(99);
+            finalScores.Add(87);
+            // For each score, if score > 85, then print if statement
+            foreach (int score in finalScores)
+            {
+                if (score > 85)
+                {
+                    Console.WriteLine("Passing test score: " + score);
+                }
+            }
+            Console.ReadLine();
+
+            // 3. Add another loop where "<=" is the comparison operator
+            // For each failScore, if failScore <= 84, print if statement
+            foreach (int failScore in finalScores)
+            {
+                if (failScore <= 84)
+                {
+                    Console.WriteLine("Your score of: " + failScore + " did not pass.");
+                }
+            }
+            Console.ReadLine();
+
+            // Part Four Assignment
+            // 1. Add list of strings where each item is unique
+            List<string> pokeList = new List<string>();
+            pokeList.Add("Charmander");
+            pokeList.Add("Bulbasaur");
+            pokeList.Add("Squirtle");
+
+            // 2. Ask user to input text to search for in the list
+            // Prints message in parentheses
+            Console.WriteLine("Here are three Pokemon. Pick a number between 0-2 to choose your starter.");
+
+
+            // 3. Loop that iterates through list + displays index of list item with matching text on the screen
+
+            int pokeSelect = Convert.ToInt32(Console.ReadLine());
+            // 4. Add code to check if user input text not on the list
+
+            // If selected number is greater than the number of indexes - 1 OR if selected number is less than 0, print this if statement
+            if (pokeSelect > pokeList.Count - 1 || pokeSelect < 0) // invalid string
+            {
+                Console.WriteLine("You didn't choose a valid number. Guess you don't want a Pokemon..");
+            }
+            else
+            {
+                Console.WriteLine("You've selected: " + pokeSelect + ", so you're Pokemon is " + pokeList[pokeSelect]);
+            }
+            // 5. Add a code that stops loop from executing once a match has been found.
+            // One loop is ran, pauses console
+            Console.ReadLine();
+
+            // Part Five Assignment
+
+            // 1. Add list of strings with at least two identical strings in the list + ask user to select text to search for in list
+            List<string> names = new List<string>() { "Brandon", "Broman", "Bruce", "Gillian", "Gillian" };
+            // Prints message asking for user to input text to search for
+            Console.WriteLine("Input name to search for in the names list");
+            string selected = Console.ReadLine();
+            // For each string name in names, perform this loop
+            for (int i = 0; i < names.Count; i++)
+            {
+                // If name starts with "B" from user input, returns name starting with that input
+                if (selected == names[i])
+                {
+                    Console.WriteLine("Match found at " + i);
+                }
+                //else
+                //{
+                //    Console.WriteLine("There's no name with that letter text in this list.");
+                //    break;
                 //}
-                //for (int m=0; m < messages.Length; m)
-                //Console.ReadLine();
+            }
+            if (!names.Contains(selected))
+            {
+                Console.WriteLine("There's no matching name in this list");
+            }
+            Console.ReadLine();
 
+            // Part Six Assignment
 
+            // 1.Create list of strings with at least two identical strings in list
+
+            List<string> grades = new List<string>() { "A", "B", "C", "D", "A", "F" };
+            List<string> gradesCopy = new List<string>();
+
+            // 2. Create foreach loop that displays message showing if string has already appeared in list
+            // For each grade in grades list, perform this loop
+            foreach (string grade in grades)
+            {
+                // if gradesCopy list contains "grade", print if statement
+                if (gradesCopy.Contains(grade))
+                {
+                    Console.WriteLine(grade + " has occured before");
+                }
+                // if gradesCopy list doesn't contain "grade", print else statement
+                else
+                {
+                    Console.WriteLine(grade + " has not occured before");
+                }
+                // Adds current item from grade list to gradesCopy. Holds copy of each previously occuring item
+                gradesCopy.Add(grade);
+
+            }
+            Console.ReadLine();
         }
-            
     }
 }
 
 
-
-            //// Asks user to select index of array using numbers 0-2
-            //Console.WriteLine("Please select an index of the Array using numbers 0-2.");
-            //int indexNum = Convert.ToInt32(Console.ReadLine());
-            //bool arrayNum = indexNum <= 2;
-            ////// Do this loop, then depending on user input, returns stringArray
-            //do
-            //{
-            //    switch (indexNum)
-            //    {
-            //        // If user inputs 0, returns stringArray[0]
-            //        case 0:
-            //            Console.WriteLine(stringArray[0]);
-            //            Console.ReadLine();
-            //            arrayNum = true;
-            //            break;
-            //        // If user inputs 1, returns stringArray[1]
-            //        case 1:
-            //            Console.WriteLine(stringArray[1]);
-            //            Console.ReadLine();
-            //            arrayNum = true;
-            //            break;
-            //        // If user inputs 2, returns stringArray[2]
-            //        case 2:
-            //            Console.WriteLine(stringArray[2]);
-            //            Console.ReadLine();
-            //            arrayNum = true;
-            //            break;
-            //        // If user inputs any other number, returns message
-            //        default:
-            //            // 3. Displays message when user selects invalid index
-            //            Console.WriteLine("Array number not valid. Please select a number between 0-2.");
-            //            indexNum = Convert.ToInt32(Console.ReadLine());
-            //            break;
-            //    }
-            //}
-            // Returns message after previous sequence loop
-            //while (!arrayNum);
-            //Console.WriteLine("Thank you for picking a valid string. Let's move on.");
-            //Console.ReadLine();
-
-            //// 2. One-Dimensional Array of Integers
-            //int[] intArray = new int[3];
-            //intArray[0] = 0;
-            //intArray[1] = 1;
-            //intArray[2] = 2;
-
-            //// Asks user to select index of array using numbers 0-2
-            //Console.WriteLine("Please select an index of the Array u sing numbers 0-2");
-            //int intNum = Convert.ToInt32(Console.ReadLine());
-            //bool intIndex = intNum <= 2;
-
-            //// Goes through loop
-            //do
-            //{
-            //    switch (intNum)
-            //    {
-            //        // If user inputs 0, returns intArray[0]
-            //        case 0:
-            //            Console.WriteLine("The value in index 0 is: " + intArray[0]);
-            //            Console.ReadLine();
-            //            intIndex = true;
-            //            break;
-            //        // If user inputs 1, returns intArray[1]
-            //        case 1:
-            //            Console.WriteLine("The value in index 1 is: " + intArray[1]);
-            //            Console.ReadLine();
-            //            intIndex = true;
-            //            break;
-            //        // If user inputs 2, returns intArray[2]
-            //        case 2:
-            //            Console.WriteLine("The value in index 2 is: " + intArray[2]);
-            //            Console.ReadLine();
-            //            intIndex = true;
-            //            break;
-            //        // If user inputs any other number, returns message
-            //        default:
-            //            // 3. Displays message when user selects invalid index
-            //            Console.WriteLine("Array number not valid. Please select a number between 0-2.");
-            //            intNum = Convert.ToInt32(Console.ReadLine());
-            //            break;
-            //    }
-            //}
-            //while (!intIndex);
-            //Console.WriteLine("Great, let's move on");
-            //Console.ReadLine();
-
-            //// 4. List of Strings
-            //List<string> intList = new List<string>();
-            //intList.Add("Hey there");
-            //intList.Add("Broman");
-            //intList.Add("What's up?");
-
-            //// Asks user to select index of array using numbers 0-2
-            //Console.WriteLine("Please select an index of the list using numbers 0-2");
-            //int listNum = Convert.ToInt32(Console.ReadLine());
-            //bool indexList = listNum <= 2;
-
-            ////// Do this loop, then depending on user input, returns intList
-            //do
-            //{
-            //    switch (listNum)
-            //    {
-            //        case 0:
-            //            Console.WriteLine(intList[0]);
-            //            Console.ReadLine();
-            //            indexList = true;
-            //            break;
-            //        case 1:
-            //            Console.WriteLine(intList[1]);
-            //            Console.ReadLine();
-            //            indexList = true;
-            //            break;
-            //        case 2:
-            //            Console.WriteLine(intList[2]);
-            //            Console.ReadLine();
-            //            indexList = true;
-            //            break;
-            //        default:
-            //            Console.WriteLine("Index doesn't exist. Try again.");
-            //            listNum = Convert.ToInt32(Console.ReadLine());
-//                        break;
-//                }
-//            }
-//            while (!indexList);
-//            Console.WriteLine("Thank you. We've reached the end of this program.");
-//            Console.ReadLine();
-//        }
-//    }
-//}
