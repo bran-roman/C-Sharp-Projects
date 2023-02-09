977ed7143848c904e8ff4ec3a86896f3d6a5b1f1
Brandon Roman <116917326+flipsun@users.noreply.github.com>
2023-02-09 13:16:04 -0800

    Adding Main Method Submission Assignment

---
+++ Main Method Submission Assignment/Main Method Submission Assignment/Operations.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method_Submission_Assignment
+{
+    public class Operations
+    {
+        // 1. Create class that'll take in an integer, create a math operation, then return answer as integer
+        public int Add(int i)
+        {
+            int input = i;
+            return input + input;
+        }
+
+        // 3. Add a second method to the class with same name that'll take in a decimal, create a different math operation for it, then return answer as integer
+        public int Add (decimal d)
+        {
+            decimal input = d;
+            int decInput = Convert.ToInt32(input);
+            return decInput * decInput;
+        }
+
+        // 5. Add third method with same name that'll take in a string, convert it to an integer if possible, and do different math operation on
+        public int Add (string s)
+        {
+            string input = s;
+            int strInput = Convert.ToInt32(input);
+            return strInput / strInput;
+        }
+    }
+}
+
+
+//namespace CallingMethodsSubmission
+//{
+//    class Program
+//    {
+//        public int Data { get; set; }
+//        static void Main(string[] args)
+//        {
+
+//            // 2. Asks user what number they want to do the math operations on
+//            Console.WriteLine("Pick a number to perform math operations on.");
+//            int askNum = Convert.ToInt32(Console.ReadLine());
+
+//            // 3. Call each method in turn, passing user input to method + Display returned integer to screen
+//            var operatorObject = new CreatedClassOne();
+//            var addResult = operatorObject.Add(askNum);
+//            var subResult = operatorObject.Subtract(askNum);
+//            var divResult = operatorObject.Divide(askNum);
+
+//            // Prints results using each method
+//            Console.WriteLine("You picked: " + askNum);
+//            Console.WriteLine("Adding 2 to " + askNum + " is: " + addResult);
+//            Console.WriteLine("Subtracting 2 by " + askNum + " is: " + subResult);
+//            Console.WriteLine("Dividing " + askNum + " by 2 is: " + divResult);
+
+//            Console.ReadLine();
+//        }
+
+//    }
+//}
+
+//namespace CallingMethodsSubmission
+//{
+//    // 1. Create class w/ 3 methods, each will take an integer and return an integer based on math operation
+//    public class CreatedClassOne
+//    {
+//        // Constructor: Value assigned to object upon creation
+//        public int Add(int data)
+//        {
+//            return data + 2;
+//        }
+//        public int Subtract(int data)
+//        {
+//            return data - 2;
+//        }
+//        public int Divide(int data)
+//        {
+//            return data / 2;
+//        }
+//    }
+//}
---
+++ Main Method Submission Assignment/Main Method Submission Assignment/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main_Method_Submission_Assignment
+{
+    class Program
+    {
+        public int Data { get; set; }
+        static void Main()
+        {
+            // 2. Instantiate the class and call the one method, passing in an integer and display results on screen
+            Console.WriteLine("Please input an integer to perform math operations on.");
+            int intNum = Convert.ToInt32(Console.ReadLine());
+            var operationObject = new Operations();
+            var addResult = operationObject.Add(intNum);
+
+            // Prints addResult
+            Console.Write("Your number: " + intNum + " + " + intNum + " = " + addResult);
+            Console.ReadLine();
+
+            // 4. Instatiate class and call second method, passing in a decimal and display results
+            Console.WriteLine("Please input a decimal to perform math operations on");
+            decimal decNum = Convert.ToDecimal(Console.ReadLine());
+            var timesResult = operationObject.Add(decNum);
+
+            // Prints timesResult
+            Console.WriteLine(decNum + " multiplied by " + decNum + " returned as an integer is " + timesResult);
+            Console.ReadLine();
+
+            // 6. Instatiate class + call third method, passing in a string equating to integer and display
+            Console.WriteLine("Please input a string to perform math operations on");
+            string strNum = Console.ReadLine();
+            var divResult = operationObject.Add(strNum);
+
+            // Prints divResult
+            Console.WriteLine(strNum + " divided by " + strNum + " returned as an integer is " + divResult);
+            Console.ReadLine();
+        }
+    }
+}
+
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace TwentyOne
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Deck deck = new Deck();
+//            int timesShuffled = 0;
+//            // deck = Shuffle(deck);
+//            // Named parameter in parentheses "deck: deck")
+//            deck = Shuffle(deck: deck, out timesShuffled, 3);
+
+//            foreach (Card card in deck.Cards)
+//            {
+//                Console.WriteLine(card.Face + " of " + card.Suit);
+//            }
+//            Console.WriteLine(deck.Cards.Count);
+//            Console.WriteLine("Times shuffled: {0}", timesShuffled);
+//            Console.ReadLine();
+
+//        }
+//        // When assigning default value, we're assuming it's 1
+//        // Creates optional parameters in this method
+//        // "out int timesShuffled" = out parameter, goes before optional parameters
+//        public static Deck Shuffle(Deck deck, out int timesShuffled, int times = 1)
+//        {
+//            for (int i = 0; i < times; i++)
+//            {
+//                List<Card> TempList = new List<Card>();
+//                Random random = new Random();
+//                while (deck.Cards.Count > 0)
+//                {
+//                    timesShuffled++;
+//                    // Creates random index, takes min and max value
+//                    int randomIndex = random.Next(0, deck.Cards.Count);
+//                    //Adds it to our TempList
+//                    TempList.Add(deck.Cards[randomIndex]);
+//                    //Function of list method that removes Random Index
+//                    deck.Cards.RemoveAt(randomIndex);
+//                }
+//                deck.Cards = TempList;
+//            }
+
+//            return deck;
+//        }
+//        //public static Deck Shuffle(Deck deck, int times)
+//        //{
+//        //    for (int i= 0; i < times; i++)
+//        //    {
+//        //        deck = Shuffle(deck);
+//        //    }
+//        //    return deck;
+//        //}
+//    }
+//}
