0efc0a071b86e377872ce63fd1d267df63a084c1
Brandon Roman <116917326+flipsun@users.noreply.github.com>
2023-02-09 12:11:18 -0800

    Adding Calling Methods Submission Assignment

---
+++ CallingMethodsSubmission/CallingMethodsSubmission/createdClassOne.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsSubmission
+{
+    // 1. Create class w/ 3 methods, each will take an integer and return an integer based on math operation
+    public class CreatedClassOne
+    {
+        // Constructor: Value assigned to object upon creation
+        public int Add(int data)
+        {
+            return data + 2;
+        }
+        public int Subtract(int data)
+        {
+            return data - 2;
+        }
+        public int Divide(int data)
+        {
+            return data / 2;
+        }
+    }
+}
