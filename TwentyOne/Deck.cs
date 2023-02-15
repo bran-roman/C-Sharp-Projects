fa6fe8a6f5634a82027669393355a327df73ec96
Brandon Roman <116917326+flipsun@users.noreply.github.com>
2023-02-14 20:47:08 -0800

    Add project files.

---
+++ TwentyOne/Deck.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    public class Deck
+    {
+        public Deck()
+        {
+            Cards = new List<Card>();
+            List<string> Suits = new List<string>() { "Clubs", "Hearts", "Diamonds", "Spades" };
+            List<string> Faces = new List<string>()
+            {
+                "Two", "Three", "Four", "Five", "Six", "Seven",
+                "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace"
+            };
+
+            foreach (string face in Faces)
+            {
+                foreach (string suit in Suits)
+                {
+                    Card card = new Card();
+                    card.Suit = suit;
+                    card.Face = face;
+                    Cards.Add(card);
+                }
+            }
+        }
+        // List data type easier to use than array
+        public List<Card> Cards { get; set; }
+
+        // When assigning default value, we're assuming it's 1
+        // Creates optional parameters in this method
+        // "out int timesShuffled" = out parameter, goes before optional parameters
+        public void Shuffle(int times = 1)
+        {
+            for (int i = 0; i < times; i++)
+            {
+                List<Card> TempList = new List<Card>();
+                Random random = new Random();
+                while (Cards.Count > 0)
+                {
+                    // Creates random index, takes min and max value
+                    int randomIndex = random.Next(0, Cards.Count);
+                    //Adds it to our TempList
+                    TempList.Add(Cards[randomIndex]);
+                    //Function of list method that removes Random Index
+                    Cards.RemoveAt(randomIndex);
+                }
+                Cards = TempList;
+            }
+        }
+    }
+        
+
+
+}
---
+++ TwentyOne/Game.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    public abstract class Game
+    {
+        private List<Player> _players = new List<Player>();
+        private Dictionary<Player, int> _bets = new Dictionary<Player, int>();
+
+
+        public List<Player> Players { get { return _players; } set { _players = value; } }
+        public string Name { get; set; }
+        public Dictionary<Player, int> Bets { get { return _bets; } set { _bets = value; } }
+
+        // Abstract method that only exists in abstract class + contains no implementation
+        // Any class inheriting this class must implement this method
+        public abstract void Play();
+
+        // What if we wanted to have our own implementation of players?
+        // Use "virtual" keyword - Virtual method in an abstract class that means
+        // This method gets inherited by inheriting class but has ability to override 
+        public virtual void ListPlayers()
+        {
+            foreach (Player player in Players)
+            {
+                Console.WriteLine(player.Name);
+            }
+        }
+    }
+}
