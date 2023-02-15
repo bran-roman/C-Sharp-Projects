8f1a6e32b91b179c73b507d7b3ac23e2f58a43d1
Brandon Roman <116917326+flipsun@users.noreply.github.com>
2023-02-15 11:13:28 -0800

    Adding DateTime Submission

---
+++ DateTime/DateTime/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DateTimeSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Prints current date/time to console
+            DateTime now = DateTime.Now;
+            Console.WriteLine(now);
+
+            // 2. Asks user for a number + returns user response as int
+            Console.WriteLine("Please enter a number.");
+            int userNum = Convert.ToInt32(Console.ReadLine());
+
+            // 3. Prints to console the exact time in X hours, X being the number the user entered
+            DateTime futureTime = now.AddHours(userNum);
+            // Returns DateTime futureTime
+            Console.WriteLine(futureTime);
+            Console.ReadLine();
+        }
+    }
+}
