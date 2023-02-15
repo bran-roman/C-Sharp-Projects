fbf24db1ef087b8b8f8bc42555756ba71192cf03
Brandon Roman <116917326+flipsun@users.noreply.github.com>
2023-02-14 17:22:31 -0800

    Adding Lambda Submission Assignment

---
+++ Lambda/Lambda/Employee.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda
+{
+    // 1. Create Employee class with following properties Id, FirstName and Last Name
+    public class Employee
+    {
+        public List<Employee> Employees = new List<Employee>();
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+
+        public void ListEmployees()
+        {
+            foreach (Employee employee in Employees)
+            {
+                Console.WriteLine($"{Id + FirstName + LastName}");
+            }
+            Console.ReadLine();
+        }
+
+        
+
+    }
+}
+
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Operators_Submission_Assignment
+//{
+//    public class Program
+//    {
+//        public static void Main(string[] args)
+//        {
+//            //Employee employee = new Employee();
+//            List<Employee> employees = new List<Employee>();
+
+//            // 3. Instantiate two objects of the Employee class + assign values to their properties
+//            // Then Compare the two Emplyoee objects using the newly overloaded operators + display the results
+
+//            // Assigns values to new Employee Class
+//            Employee employee1 = new Employee() { FirstName = "Brandon", LastName = "Roman", Id = 1 };
+//            Employee employee2 = new Employee() { FirstName = "Kristie", LastName = "Pham", Id = 2 };
+
+//            // Adds to list
+//            //employee1.Employees = new List<string>() { "Brandon" + "Roman" };
+//            employees.Add(employee1);
+//            employees.Add(employee2);
+
+//            // For each employee in employees, if IDs are the same, return if statement
+//            // Otherwise, return else statement
+//            foreach (Employee employee in employees)
+//            {
+//                // Using newly overloaded operators for if statement
+//                if (employee1.Id == employee2.Id)
+//                {
+//                    Console.WriteLine("These IDs are the same.");
+//                }
+//                // Returns else statement if previous statement was false
+//                else
+//                {
+//                    Console.WriteLine($"Employee Information: \n {employee.Id} {employee.FirstName} {employee.LastName}");
+//                }
+//            }
+//            Console.ReadLine();
+//        }
+//    }
+//}
+
+//namespace Operators_Submission_Assignment
+//{
+//    // 1. Create an Employee Class with Id, FirstName and LastName properties
+//    public class Employee
+//    {
+
+//        public int Id { get; set; }
+//        public string FirstName { get; set; }
+//        public string LastName { get; set; }
+//        public virtual List<string> Employees { get; set; }
+//        public virtual void ListEmployees()
+//        {
+//            foreach (string employee in Employees)
+//            {
+//                Employees.Add(FirstName);
+//                Employees.Add(LastName);
+//                Employees.Add(Convert.ToString(Id));
+//                Console.WriteLine(FirstName + LastName + Id);
+//                Console.ReadLine();
+//            }
---
+++ Lambda/Lambda/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // 2. Create list of at least 10 employees with two using same name "Joe"
+            // Employee employee = new Employee();
+            List<Employee> employees = new List<Employee>();
+            employees.Add(new Employee() { Id = 1, FirstName = "Joe", LastName = "Roman" });
+            employees.Add(new Employee() { Id = 2, FirstName = "Joe", LastName = "West" });
+            employees.Add(new Employee() { Id = 3, FirstName = "Brandon", LastName = "Jane" });
+            employees.Add(new Employee() { Id = 4, FirstName = "Gillian", LastName = "Nguyen" });
+            employees.Add(new Employee() { Id = 5, FirstName = "Bruce", LastName = "Kim" });
+            employees.Add(new Employee() { Id = 6, FirstName = "Chris", LastName = "Tran" });
+            employees.Add(new Employee() { Id = 7, FirstName = "Nat", LastName = "Le" });
+            employees.Add(new Employee() { Id = 8, FirstName = "Tina", LastName = "Park" });
+            employees.Add(new Employee() { Id = 9, FirstName = "Ryan", LastName = "Higa" });
+            employees.Add(new Employee() { Id = 10, FirstName = "Valky", LastName = "Rae" });
+
+            //Employee employee1 = new Employee() { Id = 1, FirstName="Joe", LastName="Roman" };
+            //Employee employee2 = new Employee() { Id = 2, FirstName = "Joe", LastName = "West" };
+            //Employee employee3 = new Employee() { Id = 3, FirstName = "Brandon", LastName = "Jane" };
+            //Employee employee4 = new Employee() { Id = 4, FirstName = "Gillian", LastName = "Nguyen" };
+            //Employee employee5 = new Employee() { Id = 5, FirstName = "Bruce", LastName = "Kim" };
+            //Employee employee6 = new Employee() { Id = 6, FirstName = "Chris", LastName = "Tran" };
+            //Employee employee7 = new Employee() { Id = 7, FirstName = "Nat", LastName = "Le" };
+            //Employee employee8 = new Employee() { Id = 8, FirstName = "Tina", LastName = "Park" };
+            //Employee employee9 = new Employee() { Id = 9, FirstName = "Ryan", LastName = "Higa" };
+            //Employee employee10 = new Employee() { Id = 10, FirstName = "Valky", LastName = "Rae" };
+
+            //employees.Add(employee1);
+            //employees.Add(employee2);
+            //employees.Add(employee3);
+            //employees.Add(employee4);
+            //employees.Add(employee5);
+            //employees.Add(employee6);
+            //employees.Add(employee7);
+            //employees.Add(employee8);
+            //employees.Add(employee9);
+            //employees.Add(employee10);
+
+            //foreach (Employee employee in employees)
+            //{
+            //    Console.WriteLine(employee.FirstName);
+            //}
+            //Console.ReadLine();
+
+            // 3. Using foreach loop, create new list of all employees with first name "joe" in comparison statement
+            // Reference property of the object you're checking
+
+
+            List<Employee> JoeList = new List<Employee>();
+            foreach (Employee employee in employees)
+            {
+                if (employee.FirstName == "Joe")
+                {
+                    JoeList.Add(employee);
+                }
+            }
+
+            // 4. Perform same action again using Lambda
+            List<Employee> JoeList1 = employees.Where(x => x.FirstName == "Joe").ToList();
+            // 5. Make list of employees with ID number greater than 5
+            List<Employee> JoeList2 = employees.Where(x => x.Id > 5).ToList();
+
+            foreach (Employee employee in JoeList2)
+            {
+                Console.WriteLine(employee.FirstName);
+            }
+            Console.ReadLine();
+        }
+    }
+}
